d9c0650d72f23fb239514f07cb888b11f6185c99
Guangling.Hastings <hastings_gl@outlook.com>
2017-02-24 13:51:12 +1100

    Add Markdown Editor Add MainBoard Layout

--- client/Components/MarkdownView/MarkdownView.jsx
+++ client/Components/MarkdownView/MarkdownView.jsx
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import './markdownview.css';
 let marked = require('marked');
 
 export default class MarkdownView extends Component {
@@ -7,7 +8,14 @@ export default class MarkdownView extends Component {
     }
 
     render() {
-        return <div className="markdown-view"
-                    dangerouslySetInnerHTML={{__html:marked(this.props.text?this.props.text:"")}}></div>;
+        return <div className="markdown-view">
+            <div className="markdown-view-btns">
+                <div className="markdown-view-btn" onClick={this.props.switchView}>
+                    <i className="material-icons">border_color</i>
+                </div>
+            </div>
+            <div className="markdown-view-content"
+                 dangerouslySetInnerHTML={{__html:marked(this.props.text?this.props.text:"")}}></div>
+        </div>;
     }
 }
---
+++ client/Components/Note/MainBoard.jsx
@@ -0,0 +1,31 @@
+import React,{Component} from 'react';
+import CheckList from '../CheckList/CheckList.jsx';
+import './mainboard.css';
+
+export default class MainBoard extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<div className="main-board">
+            <div className="main-board-btns">
+                <div className="main-board-btn" onClick={this.props.switchView}>
+                    <i className="material-icons">assignment</i>
+                </div>
+            </div>
+            <div className="note">
+                <div className="cue-column">
+                    CUE CUE CUE
+                </div>
+                <div className="note-content">
+                    {/*<CheckList></CheckList>*/}
+                    NOTE NOTE
+                </div>
+            </div>
+            <div className="summary">
+                SUMMARY SUMMARY SUMMARY
+            </div>
+        </div>);
+    }
+}
